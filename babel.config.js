78331bdecade88147dc8ef1da2df4942e33f67b1
melodynnadi <melodynnadi2@gmail.com>
2026-05-01 14:15:30 -0500

    fix: remove missing module-resolver plugin from babel config

--- babel.config.js
+++ babel.config.js
@@ -5,16 +5,5 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['.'],
-          alias: { '@': '.' },
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        },
-      ],
-    ],
   };
 };
-
