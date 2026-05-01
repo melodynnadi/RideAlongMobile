a0c9f7bac1462135f9511fcd55c9b24ca207c3c2
melodynnadi <melodynnadi2@gmail.com>
2026-05-01 13:05:26 -0500

    fix: add babel module-resolver for @/ path alias and fix eas.json schema

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,16 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: { '@': '.' },
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        },
+      ],
+    ],
   };
 };
+
