15033b57e2e2e9b1a190739e120eee9cc19dc2e8
Melody Nnadi <melodynnadi2@gmail.com>
2026-09-10 18:26:44 -0500

    Bump app version to 1.0.1 to match App Store Connect submission

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => ({
   expo: {
     name: "RideAlong",
     slug: "RideAlongMobile",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/ridealonglogo.png",
     scheme: "ridealong",
